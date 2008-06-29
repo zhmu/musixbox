1adb692238eb09936411d1bda47345f74c6ede56
Rink Springer <rink@rink.nu>
2008-06-29 21:49:43 +0200

    Remove obsolete code from main.cc; it only confuses people and isn't used anymore.
    Noticed by:	dwight

--- src/main.cc
+++ src/main.cc
@@ -11,26 +11,11 @@
 #include "output_null.h"
 #include "visualize_spectrum.h"
 
-InputFile* input;
-Output* output;
-Decoder* decoder;
-Visualizer* vis;
-Interaction* interaction;
-Interface* interface;
-
 int
 main(int argc, char** argv)
 {
-	//output = new OutputAO();
-	output = new OutputNull();
-	vis = new SpectrumVisualizer();
-
-	input = new InputFile();
-	const char* file = "mp3/music.mp3";
-	if (argc == 2)
-		file = argv[1];
-	if (!input->open(file))
-		err(1, "input");
+	Interaction* interaction;
+	Interface* interface;
 
 	interaction = new InteractionSDL();
 	interface = new Interface(interaction);

cb29f67b99d3c8fb8f3edcb4938d2b72224736d4
Rink Springer <rink@rink.nu>
2008-06-29 21:31:37 +0200

    *oops* Add missing info_ogg.cc file, which should have been in the previous commit(s) :-/
    Noticed by:		dwight

---
+++ src/info_ogg.cc
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "decode_ogg.h"
+#include "info_ogg.h"
+
+int
+InfoOgg::load(const char* fname)
+{
+	struct vorbis_comment* c = (reinterpret_cast<DecoderOgg*> (decoder))->getComments();
+
+	for (int i = 0; i < c->comments; i++) {
+		char* ptr = strchr(c->user_comments[i], '=');
+		if (ptr == NULL)
+			continue;
+
+		if (!strncasecmp(c->user_comments[i], "TITLE", ptr - c->user_comments[i])) {
+			title = strdup(ptr + 1);
+		}
+		if (!strncasecmp(c->user_comments[i], "ARTIST", ptr - c->user_comments[i])) {
+			artist = strdup(ptr + 1);
+		}
+		if (!strncasecmp(c->user_comments[i], "ALBUM", ptr - c->user_comments[i])) {
+			album = strdup(ptr + 1);
+		}
+	}
+
+	return 1;
+}

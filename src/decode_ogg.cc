a1ccb0b048af704c57f334cf8cffe4a127841280
Rink Springer <rink@rink.nu>
2008-08-31 17:33:54 +0200

    Implement seeking and position-telling in the Ogg decoder - this is needed to obtain the song length.

--- src/decode_ogg.cc
+++ src/decode_ogg.cc
@@ -15,11 +15,38 @@ ov_wrap_read(void* ptr, size_t size, size_t nmemb, void* datasource)
 	return (input->read((char*)ptr, size * nmemb) / size);
 }
 
+int
+ov_wrap_seek(void* datasource, ogg_int64_t offset, int whence)
+{
+	Input* input = (Input*)datasource;
+	switch (whence) {
+		case SEEK_CUR: /* seek from current position */
+		               offset += input->getCurrentPosition();
+		               break;
+		case SEEK_END: /* seek from endposition */
+		               offset = input->getLength() - offset;
+		               break;
+		case SEEK_SET: /* go there, now */
+		               break;
+		      default: /* ??? */
+		               return -1;
+		               
+	}
+	return input->seek(offset) ? 0 : -1;
+}
+
+long
+ov_wrap_tell(void* datasource)
+{
+	Input* input = (Input*)datasource;
+	return input->getCurrentPosition();
+}
+
 ov_callbacks ov_input_wrapper = {
 	ov_wrap_read,
+	ov_wrap_seek,
 	NULL,
-	NULL,
-	NULL
+	ov_wrap_tell
 };
 
 DecoderOgg::~DecoderOgg()

aef5a656a58c8b90f7d0293484f7d842838e2754
Rink Springer <rink@rink.nu>
2008-09-07 15:20:21 +0200

    Update the playlist menu to confirm to the new API.

--- src/musixcurses/menuplaylist.cc
+++ src/musixcurses/menuplaylist.cc
@@ -4,9 +4,9 @@ std::string
 MenuPlaylist::getItem(unsigned int num)
 {
 	if (playlist->getCurrentPlayItem() == num)
-		return " * " + playlist->getItems()[num]->getResource();
+		return " * " + playlist->getItems()[num]->getDisplayText();
 	else
-		return "   " + playlist->getItems()[num]->getResource();
+		return "   " + playlist->getItems()[num]->getDisplayText();
 }
 
 unsigned int

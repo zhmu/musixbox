38e4e0e65b9c4777c810d728397030702e9dea93
Rink Springer <rink@rink.nu>
2008-09-20 11:09:34 +0200

    If the image control is bigger than the image itself, center it within this space.

--- lib/ui/image.cc
+++ lib/ui/image.cc
@@ -6,9 +6,12 @@ Image::draw()
 	if (image == NULL)
 		return;
 
+	unsigned int drawX = x + (width -  8) / 2;
+	unsigned int drawY = y + (height - 8) / 2;
+
 	for (unsigned int i = 0; i < 8; i++) {
 		for (unsigned int j = 0; j < 8; j++) {
-			putpixel(i + x, j + y, (image[i] & (1 << j)) ? 1 : 0);
+			putpixel(i + drawX, j + drawY, (image[i] & (1 << j)) ? 1 : 0);
 		}
 	}
 }

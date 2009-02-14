032816cf556b739d8dc89a207dccc4417130be52
Rink Springer <rink@rink.nu>
2009-02-14 13:34:53 +0100

    Rework the font format/tools:
    - Draw column-by-column now.
      The original idea was to put the font data in the AVR (which would benefit from row-by-row), but this isn't feasible size-wise so we just drop that requirement as it simplifies code immensely.
    - Rework font conversion
      The previous code didn't allocate enough space for the temporary buffer, so it could segfault (this happens especially on Linux). We no longer convert the font to a temporary buffer, but convert it in a single go.
    - Rework font dimensions
      Any height * width font should work now, as long as width <= 8. A larger font looks ugly on the LCD anyway, and it greatly simplifies the code to just limit to width of 8.

--- include/ui/font.h
+++ include/ui/font.h
@@ -1,6 +1,19 @@
+#ifndef __FONT__
+#define __FONT__
+
 struct CHARACTER {
 	int height, width, yshift, advance_x;
 	const char* data;
 };
 
-extern struct CHARACTER theFont[];
+struct FONT {
+	int height;
+	struct CHARACTER* chars;
+};
+
+extern struct FONT font8;
+extern struct FONT font12;
+
+#define DEFAULT_FONT font12
+
+#endif /* __FONT__ */
--- tools/convert_font.cc
+++ tools/convert_font.cc
@@ -1,12 +1,13 @@
 #include <sys/types.h>
 #include <ctype.h>
 #include <err.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
-#define PT_SIZE 8
+unsigned int font_size;
 
 FT_Library ftlib;
 FT_Face face;
@@ -16,7 +17,6 @@ loadfont(const char* fname)
 {
 	FILE* f;
 	unsigned long len;
-	char* buf;
 
 	if (FT_New_Face(ftlib, fname, 0, &face))
 		errx(1, "unable to initialize load font");
@@ -24,23 +24,42 @@ loadfont(const char* fname)
 	if (!FT_IS_SCALABLE(face))
 		errx(1, "font is not scalable");
 
-	if (FT_Set_Char_Size(face, 0, PT_SIZE * 64, 0, 0))
+	if (FT_Set_Char_Size(face, 0, font_size * 64, 0, 0))
 		errx(1, "can't request font size");
+}
+
+void
+usage()
+{
+	fprintf(stderr, "usage: conver_font font.ttf size\n");
+	exit(EXIT_FAILURE);
 }
 
 int
 main(int argc, char* argv[])
 {
+	char* ptr;
+	if (argc < 2)
+		usage();
+
+	font_size = strtoul(argv[2], &ptr, 10);
+	if (font_size == 0 || *ptr != '\0') {
+		fprintf(stderr, "unable to parse number '%s'\n", argv[2]);
+		usage();
+	}
+
 	FT_Error error = FT_Init_FreeType(&ftlib);
 	if (error)
 		errx(1, "unable to initialize freetype library");
 
-	loadfont("/usr/local/lib/X11/fonts/bitstream-vera/Vera.ttf");
+	//loadfont("/usr/local/lib/X11/fonts/bitstream-vera/Vera.ttf");
+	//loadfont("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansMono.ttf");
+	loadfont(argv[1]);
 
 	printf("/* This file was automatically generated - do not edit! */\n");
 	printf("#include \"ui/font.h\"\n");
 	printf("\n");
-	printf("struct CHARACTER theFont[] = {\n");
+	printf("static struct CHARACTER fontData%u[] = {\n", font_size);
 	for (FT_ULong ch = 0; ch <= 255; ch++ ) {
 		int charidx = FT_Get_Char_Index(face, ch);
 		if (charidx == 0) {
@@ -55,51 +74,48 @@ main(int argc, char* argv[])
 		FT_GlyphSlot slot = face->glyph;
 
 		/*
-		 * We construct an in-memory rendering of the charachter and
-		 * scrape all bits together on a per-column basis.
+		 * XXX: limiting ourselves to chars of maximally 8 pixels wide
+		 * simplies the code considerably, so do that for now...
 		 */
-		char* buf = (char*)malloc(slot->bitmap.rows * slot->bitmap.width);
-		if (buf == NULL)
-			err(1, "can't allocate memory");
+		if (slot->bitmap.width > 8)
+			abort();
 
-		/* Fill a buffer with the content */
-		for (int j = 0 ; j < slot->bitmap.rows; j++) {
-			for (int i = 0; i <= slot->bitmap.width / 8; i++) {
-				unsigned char* ptr = slot->bitmap.buffer +
-					(j * slot->bitmap.pitch + i);
-				for (int v = 0; v < 8; v++) {
-					buf[j * slot->bitmap.width + (i * 8) + v] = (*ptr & (1 << (7 - v))) ? 1 : 0;
-				}
-			}
-		}
 		/* C will parse a \ as: continue on the next line, so avoid it :-/ */
 		printf((isprint(ch) && (ch != '\\')) ? "\t// %c\n" : "\t// %u\n", ch);
 
 		if (ch != 0x20) {
 			printf("\t{ %u, %u, %i, %u, \"", slot->bitmap.rows, slot->bitmap.width, slot->bitmap_top, slot->advance.x >> 6);
-			for (int i = 0; i < slot->bitmap.width; i++) {
-				uint8_t ch = 0;
-				for (int j = 0 ; j < slot->bitmap.rows; j++) {
-					ch >>= 1;
-					if (buf[j * slot->bitmap.width + i])
-						ch |= 0x80;
-						if (j > 8 && j % 8 == 0) printf("\\x%02x", ch);
+			for (int j = 0; j < slot->bitmap.rows; j++) {
+				for (int i = 0; i <= 0 /* XXX: this works due to the 8-width-limit */; i++) {
+					unsigned char* ptr = slot->bitmap.buffer +
+						(j * slot->bitmap.pitch + i);
+
+					/*
+					 * Once rendered by FreeType, the bits
+					 * are ordered exactly the opposite of
+					 * how we would like them, so flip 'm.
+					 */
+					uint8_t ch = 0;
+					for (int b = 0; b < 8; b++) {
+						if (*ptr & (1 << (8 - b)))
+							ch |= (1 << b);
+					}
+					printf("\\x%02x", ch);
 				}
-				printf("\\x%02x", ch);
 			}
 			printf("\" },\n");
 		} else {
 			/* Most font files do not provide a space, so roll our own! */
-			printf("\t{ %u, %u, 0, %u, \"", PT_SIZE, PT_SIZE / 2, slot->advance.x >> 6);
-			for (int i = 0; i < PT_SIZE / 2; i++)
+			printf("\t{ %u, %u, 0, %u, \"", font_size, font_size / 2, (slot->advance.x >> 6) / 2);
+			for (int i = 0; i < font_size; i++)
 				printf ("\\x00");
 			printf("\" },\n");
 		}
-
-		free(buf);
 	}
 	printf("};\n");
-
+	printf("\n");
+	printf("struct FONT font%u = { %u, fontData%u };\n",
+	 	font_size, font_size, font_size);
 
 	FT_Done_Face(face);
 	FT_Done_FreeType(ftlib);
